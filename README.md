713d74b69bf804ecf80eb6316ac42bb015dd5865
Andrew Tomko <tomand285@gmail.com>
2017-12-22 21:40:26 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# makeTextures.js #
+
+This is a Node.js program designed to create a file to hold the base64 data of images intended to be used with webGL.
+
+### What is this repository for? ###
+
+I created this for CS 559 Computer Graphics at UW - Madison as a way to help me auto create a file of textures.
+The output format is based off of [this tool](http://graphics.cs.wisc.edu/Courses/559-f2015/Services/imagebundler/bundler.html).
+
+### How do I get set up? ###
+
+* You must have [Node.js](https://nodejs.org/en/) downloaded onto your computer in order for this program to run.
+* Either copy or download makeTextures.js and run using Node.js.
+* Save a copy of this program in the directory of your textures and run via the terminal:
+``
+node maketextures.js
+``
+* The new file will be called:
+``
+textures.js
+``
+
+###How to use textures.js in your program? ###
+* In html file:
+``
+<script src="textureFolder/textures.js"></script>
+``
+* In the texture part of your code:
+```
+var image = new Image();
+image.crossOrigin = "anonymous";
+image.src = LoadedImageFiles["myTexture.jpg"];
+image.onload = function(){
+//TODO: add texture code here
+}
+```
+
+### Contribution guidelines ###
+
+* If you would like to make edits, please create a pull request or make an new issue.
+
+### Who do I talk to? ###
+
+* Andrew Tomko
+* tomand285@gmail.com
---
+++ makeTextures.js
@@ -0,0 +1,85 @@
+/**
+ * @author Andrew Tomko
+ * https://github.com/
+ *
+ * This is a Node.js program for converting all image files in the current dir into one file called textues.js to hold base64 data
+ */
+
+var fs = require('fs');
+var path = require('path');
+ 
+if (process.argv.length != 2) {
+    console.log("Usage: node " + path.basename(__filename));
+    process.exit(-1);
+}
+ 
+//current directory
+var path = ".";
+var imgList = new Array();
+
+//JSON data of file types allowed, add new types here.
+var mimeType = 	[
+					{"ext":"jpg", "mime": "image/jpeg"},
+					{"ext":"jpeg", "mime": "image/jpeg"},
+					{"ext":"png", "mime": "image/png"},
+					{"ext":"gif", "mime": "image/gif"},
+				];
+
+/**
+ *	Returns the file as a base64 string
+ *	@param {string} file - name of file	
+ *	@returns {string} string of base64 data
+ */
+function base64_encode(file){
+	var bitmap = fs.readFileSync(file);
+	return new Buffer(bitmap).toString('base64');
+}
+
+/**
+ *	Returns the index of the JSON object
+ *	@returns {int} index of JSON object
+ */
+function indexOfJSON(string){
+	for(var i=0;i<mimeType.length;i++){
+		if(mimeType[i].ext == string)
+			return i;
+	}
+	return -1;
+}
+ 
+/**
+ *	Creates an imgList array of all files in directory
+ *	@param {string} path - directory to be scanned
+ *	@callback {err, files} files - an array of files in the directory
+ */
+fs.readdir(path, function(err, files) {	
+	console.log("Reading Directory...");
+    for (var i=0; i<files.length; i++) {
+    	if(files[i].indexOf('.') == -1)
+    		continue;
+    	imgList[i] = new Object();
+    	imgList[i].fullName = files[i];
+    	imgList[i].name = files[i].split('.')[0];
+    	imgList[i].ext = (files[i].split('.')[1]).toLowerCase();
+    	imgList[i].base64 = base64_encode(files[i]);
+    }
+});
+
+//creates a new file called "texture.js" or overwrites existing file
+var stream = fs.createWriteStream("textures.js");
+
+/**
+ *	Writes to file all the image data
+ */
+stream.once('open', (fd) => {
+	console.log("Writing to file...");
+	stream.write("var LoadedImageFiles = LoadedImageFiles || {};\n");
+	for(var i=0; i<imgList.length; i++){
+		if(indexOfJSON(imgList[i].ext) != -1){
+			stream.write("LoadedImageFiles[\""+imgList[i].fullName+"\"] = \"data:"+ mimeType[indexOfJSON(imgList[i].ext)].mime +";base64,"+imgList[i].base64+"\"\n");
+			console.log("   LoadedImageFiles[\""+imgList[i].fullName+"\"] is finished");
+		}
+	}
+	stream.end();
+	console.log("textures.js is ready");
+});
